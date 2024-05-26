94d7875c2a5dfa9758e199780a9220b8ba9afac6
olya <fgfg>
2024-05-26 22:19:51 +0300

    hometask1

---
+++ src/Main.kt
@@ -0,0 +1,72 @@
+fun main() {
+
+
+    var endProgramm: Boolean = false
+
+    while(!endProgramm) {
+        println("Введите команду:")
+        val command: String? = readLine()
+
+        if(command != null) {
+            when (command) {
+                "exit" -> endProgramm = true
+                "help" -> {
+                    println("exit - завершить программу")
+                    println("help - показать список команд")
+                    println("add <Имя> phone <Номер телефона> - добавить номер телефона пользователя")
+                    println("add <Имя> email <Адрес электронной почты> - добавить email пользователя")
+                }
+                else -> {
+                    if(command.contains("add") && command.contains("phone")) {
+                        addPhone(command)
+                    } else if (command.contains("add") && command.contains("email")) {
+                        addEmail(command)
+                    } else {
+                        println("Неккоректный формат команды")
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun addPhone(str: String) {
+    val words = str.split(" ")
+    var nums = words[3].subSequence(1, words[3].length - 1).toString()
+    if(words[3].first() == '+' && nums.toIntOrNull() != null) {
+
+        println("Имя: ${words[1]}, телефон: ${words[3]}")
+    } else {
+        println("Неккоректный формат номера телефона")
+    }
+}
+
+fun addEmail(str: String) {
+    var words = str.split(" ")
+
+    if(words[3].contains('@') && words[3].contains('.')) {
+        var delim1 = words[3].indexOf("@")
+        var delim2 = words[3].indexOf(".")
+        println(delim1)
+        println(delim2)
+        var parts = Array(3, {""})
+        var correct: Boolean
+
+        parts[0] = words[3].subSequence(0, delim1).toString()
+        parts[1] = words[3].subSequence(delim1 + 1, delim2).toString()
+        parts[2] = words[3].subSequence(delim2 + 1, words[3].length).toString()
+
+        for(i in parts) {
+            if(i.length < 2) {
+                correct = false
+                return
+            } else {
+                println("Некорректный адрес электронной почты")
+            }
+        }
+
+        println("Имя: ${words[1]}, email: ${words[3]}")
+    } else {
+        println("Некорректный адрес электронной почты")
+    }
+}
